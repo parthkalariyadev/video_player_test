1c7a71d686d297ab48538f0284cf44cf5df95cac
Parth Kalariya <>
2023-02-16 18:52:45 +0530

    test

--- video_player/video_player_android/android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayer.java
+++ video_player/video_player_android/android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayer.java
@@ -74,7 +74,7 @@ final class VideoPlayer {
 
     ExoPlayer exoPlayer = new ExoPlayer.Builder(context).build();
 
-    Uri uri = Uri.parse(/*dataSource*/"https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4");
+    Uri uri = Uri.parse(/*dataSource*/"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4");
     DataSource.Factory dataSourceFactory;
 
     if (isHTTP(uri)) {
