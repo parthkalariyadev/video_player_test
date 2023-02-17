f570e511545f1f78ee47aef43e387da242cefb0d
Parth Kalariya <>
2023-02-17 11:26:53 +0530

    test

--- video_player/video_player/example/lib/main.dart
+++ video_player/video_player/example/lib/main.dart
@@ -28,7 +28,7 @@ class _VideoAppState extends State<VideoApp> {
   void initState() {
     super.initState();
     _controller = VideoPlayerController.network(
-        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4')
+        'https://storage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4')
       ..initialize().then((_) {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
